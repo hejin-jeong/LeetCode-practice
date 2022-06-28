6952809aed463f11a214e65612e256d8cf730e20
hejin-jeong <92611620+hejin-jeong@users.noreply.github.com>
2022-06-27 21:44:05 -0400

    Time: 56 ms (34.26%), Space: 14.1 MB (78.38%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -4,13 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: int
-        """
-        cur = 0
-        while cur < len(nums):
-            if nums[cur] == target or nums[cur] > target:
-                return cur
-            
-            cur += 1
-            
-        return cur
-        
+        """  
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+                
+        return l
