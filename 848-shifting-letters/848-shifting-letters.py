5e8ff0abd80a6ea73292bb8649679f67a8f4cc2e
hejin-jeong <92611620+hejin-jeong@users.noreply.github.com>
2022-06-29 21:41:31 -0400

    Time: 4561 ms (6.98%), Space: 24.8 MB (46.51%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.py
+++ 848-shifting-letters/848-shifting-letters.py
@@ -10,7 +10,7 @@ class Solution(object):
         p = len(s)-1
         while p >= 0:
             value = (sum + ord(s[p]) + shifts[p] - 97)%26 + 97
-            # new_string.insert(0, chr(value))
+            # new_string.insert(0, chr(value)) works for arrays
             new_string = chr(value) + new_string
             sum += shifts[p]
             p -= 1
