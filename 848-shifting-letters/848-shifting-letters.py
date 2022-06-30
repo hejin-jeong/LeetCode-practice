e2a1fea85ba17a3e532458c65edacbdeff0d2ec6
hejin-jeong <92611620+hejin-jeong@users.noreply.github.com>
2022-06-29 21:41:07 -0400

    Time: 4472 ms (9.30%), Space: 25 MB (37.21%) - LeetHub

---
+++ 848-shifting-letters/848-shifting-letters.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def shiftingLetters(self, s, shifts):
+        """
+        :type s: str
+        :type shifts: List[int]
+        :rtype: str
+        """
+        new_string = ""
+        sum = 0
+        p = len(s)-1
+        while p >= 0:
+            value = (sum + ord(s[p]) + shifts[p] - 97)%26 + 97
+            # new_string.insert(0, chr(value))
+            new_string = chr(value) + new_string
+            sum += shifts[p]
+            p -= 1
+
+        return new_string
+        
