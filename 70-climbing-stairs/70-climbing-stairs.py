869427779e07f5c0b855dde7c6f25cc01321403c
hejin-jeong <92611620+hejin-jeong@users.noreply.github.com>
2022-07-02 15:06:19 -0400

    Time: 22 ms (64.71%), Space: 13.5 MB (34.80%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        steps = [0]*(n+1)
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        steps[1] = 1
+        steps[2] = 2
+        
+        for i in range(3,n+1):
+            steps[i] = steps[i-1] + steps[i-2]
+            
+        return steps[n]
+        
+        
+        # Below solutions are O(2^n)
+        
+        # if n == 0:
+        #     return 1
+        # elif n < 0:
+        #     return 0
+        # return self.climbStairs(n-1) + self.climbStairs(n-2)
+            
+        
+        
+        
+#         def helper(step):
+#             if step == n:
+#                 return 1
+#             if step > n:
+#                 return 0
+            
+#             one = helper(step+1)
+#             two = helper(step+2)
+            
+#             return one + two
+        
+        
+#         return helper(0)
+        
