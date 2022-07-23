032ead5bffaa78bea4865304bc17ca35554bfc2f
hejin-jeong <92611620+hejin-jeong@users.noreply.github.com>
2022-07-23 14:33:12 -0400

    Time: 27 ms (50.42%), Space: 13.6 MB (14.94%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -4,21 +4,51 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        phone = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
-        # mapper=["0","1","abc","def","ghi", "jkl","mno","pqrs","tuv","wxyz"]
-        res= []
+#         phone = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+#         # mapper=["0","1","abc","def","ghi", "jkl","mno","pqrs","tuv","wxyz"]
+#         res= []
         
-        if not digits:
-            return res
+#         if not digits:
+#             return res
         
-        res.append("")
+#         res.append("")
         
-        for digit in digits:
-            temp=list()
-            chars = phone[digit]
-            for r in res:
-                for cha in chars:
-                    temp.append(r+cha)
+#         for digit in digits:
+#             temp=list()
+#             chars = phone[digit]
+#             for r in res:
+#                 for cha in chars:
+#                     temp.append(r+cha)
                     
-            res = temp
-        return res
+#             res = temp
+#         return res
+
+
+
+        if len(digits) == 0: 
+            return []
+
+        # Map all the digits to their corresponding letters
+        letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", 
+                   "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+
+        def backtrack(index, path):
+            # If the path is the same length as digits, we have a complete combination
+            if len(path) == len(digits):
+                combinations.append("".join(path))
+                return # Backtrack
+
+            # Get the letters that the current digit maps to, and loop through them
+            possible_letters = letters[digits[index]]
+            for letter in possible_letters:
+                # Add the letter to our current path
+                path.append(letter)
+                # Move on to the next digit
+                backtrack(index + 1, path)
+                # Backtrack by removing the letter before moving onto the next
+                path.pop()
+
+        # Initiate backtracking with an empty path and starting index of 0
+        combinations = []
+        backtrack(0, [])
+        return combinations
