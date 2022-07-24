834e05b21bbecb7f10738c9cf50733cf011249cb
hejin-jeong <92611620+hejin-jeong@users.noreply.github.com>
2022-07-24 16:41:29 -0400

    Time: 15 ms (91.69%), Space: 13.6 MB (11.93%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -10,13 +10,32 @@ class Solution(object):
         if n == 2:
             return 2
         
-        steps[1] = 1
-        steps[2] = 2
+        pp = 1
+        p = 2
         
         for i in range(3,n+1):
-            steps[i] = steps[i-1] + steps[i-2]
+            t = p + pp
+            pp = p
+            p = t
             
-        return steps[n]
+        return t
+    
+    
+    
+        
+#         steps = [0]*(n+1)
+#         if n == 1:
+#             return 1
+#         if n == 2:
+#             return 2
+        
+#         steps[1] = 1
+#         steps[2] = 2
+        
+#         for i in range(3,n+1):
+#             steps[i] = steps[i-1] + steps[i-2]
+            
+#         return steps[n]
         
         
         # Below solutions are O(2^n)
